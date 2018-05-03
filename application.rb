31ce05a1fef4873bd2417d93ecd47e0be19ff04e
David Markel <davidgmarkel@gmail.com>
2018-05-03 15:38:55 +0000

    automatically backed up by learn

--- application.rb
+++ application.rb
@@ -2,9 +2,8 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World.  My name is name."
+    resp.write "Hello, my name is name."
     resp.finish
   end
 
 end
-
